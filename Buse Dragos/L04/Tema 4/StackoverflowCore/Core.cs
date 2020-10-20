770d617bf62573ddd519ebcb6f704dd572d954b0
Buse Dragos <busedragos@yahoo.com>
2020-10-20 11:53:53 -0700

    Tema 4

---
+++ Buse Dragos/L04/Tema 4/StackoverflowCore/Core.cs
@@ -0,0 +1,16 @@
+﻿using Primitives.IO;
+using StackoverflowSchema;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+
+
+namespace StackoverflowCore
+{
+    public class Core
+    {
+        public static Port<ICreateQuest> CreateQuest(string titlu, string intrebare, string tag) => NewPort<CMD, ICreateQuest>(new CMD(titlu, intrebare, tag));
+    }
+}
---
+++ Buse Dragos/L04/Tema 4/StackoverflowSchema/CMD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackoverflowSchema
+{
+    public class CMD
+    {
+        public CMD(string titlu, string intrebare, string tag)
+        {
+            Titlu = titlu;
+            Intrebare = intrebare;
+            Tag = tag;
+        }
+        [Required]
+        public string Titlu { get; }
+        [Required]
+        public string Intrebare { get; }
+        [Required]
+        public string Tag
+        {
+            get;
+        }
+    }
+}
